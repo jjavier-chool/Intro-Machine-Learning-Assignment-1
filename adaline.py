104b5176cd43ecb7168a2ec4eb80e56ea135a731
Pranithaachanta <pranithaachanta@gmail.com>
2026-02-12 10:06:34 -0700

    Refactor AdalineGD class implementation

--- adaline.py
+++ adaline.py
@@ -6,10 +6,7 @@ class AdalineGD:
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        # Add bias column (column of ones)
         X = np.hstack([X, np.ones((X.shape[0], 1))])
-
-        # Initialize weights INCLUDING bias weight
         self.w_ = np.zeros(X.shape[1])
         self.losses_ = []
 
@@ -30,3 +27,4 @@ class AdalineGD:
     def predict(self, X):
         X = np.hstack([X, np.ones((X.shape[0], 1))])
         return np.where(self.net_input(X) >= 0.0, 1, -1)
+
